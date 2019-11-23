c86c8b27ae56f7891188da7743b0f28568564b14
Gabriel A. Zorrilla <gabriel@zorrilla.me>
2019-11-23 20:54:11 -0300

    Implementing css grids for layout.

--- code.js
+++ code.js
@@ -1,29 +1,56 @@
 function init() {
   localStorage.setItem("jsonData", JSON.stringify(templates.whfrpg4e));
-  let characterData = JSON.parse(localStorage.getItem("jsonData"));
-  generateForm(characterData);
+  let character = JSON.parse(localStorage.getItem("jsonData"));
+  generateForm(character);
 }
 
-function saveData() {
-  for (i in document.getElementById("character")) {
-    console.log(i);
-  }
-}
-
-function generateForm(characterData) {
+function generateForm(character) {
   let sheet = document.getElementById("character")
-  for (const field in characterData.basics) {
-    let input = document.createElement("input");
-    input.name = field;
-    input.type = characterData.basics[field].type;
-    sheet.appendChild(input);
+  for (const section in character.sheet) {
+    console.log(section);
+    let sectionDiv = document.createElement("div");
+    sectionDiv.id = section;
+    for (const field in character.sheet[section]) {
+      let f = character.sheet[section][field];
+      let container = document.createElement("div");
+      container.className = "field";
+      container.appendChild(createInput(field, f));
+      container.appendChild(createLabel(f));
+      sectionDiv.appendChild(container);
+    }
+    sheet.appendChild(sectionDiv);
   }
   let button = document.createElement("input");
   button.type = "submit";
   button.name = "save";
   button.text = "save";
   button.onclick = saveData;
-  sheet.appendChild(button);
+  //  sheet.appendChild(button);
+  console.log(sheet);
 }
+
+
+function saveData() {
+
+}
+
+function createInput(field, f) {
+  let input = document.createElement("input");
+  input.name = field;
+  input.type = f.type;
+  input.size = f.size;
+  input.style.width = f.size + "rem";
+  input.maxLength = f.size;
+  return input;
+}
+
+function createLabel(f) {
+  let label = document.createElement("label");
+  label.innerHTML = f.label;
+  return label;
+}
+
+
+
 
 window.onload = init;
--- templates.js
+++ templates.js
@@ -1,22 +1,92 @@
 let templates = {
   "whfrpg4e": {
-    "basics": {
-      "name": {
-        "value": "Lore",
-        "type": "text"
+    "sheet": {
+      "basics":
+      {
+        "name": {
+          "value": "",
+          "type": "text",
+          "size": "32",
+          "label": "Name"
+        },
+        "species": {
+          "value": "",
+          "type": "text",
+          "size": "16",
+          "label": "Species"
+        },
+        "class": {
+          "value": "",
+          "type": "text",
+          "size": "16",
+          "label": "Class"
+        },
+        "career": {
+          "value": "",
+          "type": "text",
+          "size": "16",
+          "label": "Career"
+        },
+        "career_level": {
+          "value": "",
+          "type": "number",
+          "size": "2",
+          "label": "Level"
+        },
+        "career_path": {
+          "value": "",
+          "type": "text",
+          "size": "32",
+          "label": "Career Path"
+        },
+        "status": {
+          "value": "",
+          "type": "text",
+          "size": "16",
+          "label": "Status"
+        },
+        "age": {
+          "value": "",
+          "type": "number",
+          "size": "3",
+          "label": "Age"
+        },
+        "height": {
+          "value": "",
+          "type": "number",
+          "size": "3",
+          "label": "Height"
+        },
+        "hair": {
+          "value": "",
+          "type": "text",
+          "size": "12",
+          "label": "Hair"
+        },
+        "eyes": {
+          "value": "",
+          "type": "text",
+          "size": "12",
+          "label": "Eyes"
+        },
       },
-      "species": {
-        "value": "sasasa",
-        "type": "text"
-      },
-      "class": {
-        "value": "sasa",
-        "type": "text"
-      },
-      "age": {
-        "value": "11",
-        "type": "number"
+      "characteristics": {
+        "ws": {
+          "value": "",
+          "type": "number",
+          "size": "2",
+          "label": "WS"
+        },
+        "bs": {
+          "value": "",
+          "type": "number",
+          "size": "2",
+          "label": "BS"
+        }
       }
+    },
+    "style": {
+      "font": "CormorantInfant-Medium.ttf"
     },
     "metadata": {
       "character": {
@@ -26,11 +96,5 @@ let templates = {
         "gamesystem": ""
       }
     },
-  },
-  "system": {
-    "version": "",
-    "updated": "",
-    "notes": "",
-    "author": "@GabrielZorrilla"
   }
 };
