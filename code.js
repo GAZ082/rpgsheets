23b53a30f0253d2e8502cad38ca1c5fcd1babc8b
Gabriel A. Zorrilla <gabriel@zorrilla.me>
2019-12-01 23:43:05 -0300

    Finished calc engine.

--- code.js
+++ code.js
@@ -3,39 +3,51 @@
 
 let app = {};
 app.template = {}
-app.crunch = []
+app.data = []
+app.calc = []
 
 function init() {
-  localStorage.setItem("jsonData", JSON.stringify(templates.whfrpg4e));
-  app.template = JSON.parse(localStorage.getItem("jsonData"));
-  generateCrunch(app.template.sheet);
+  // fetch('http://localhost/templates.js')
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     console.log(data)
+  //   })
+  //   .catch(err => console.error(err));
+  localStorage.setItem("templateData", JSON.stringify(templates.whfrpg4e));
+  app.template = JSON.parse(localStorage.getItem("templateData"));
+  cacheData(app.template.sheet)
   generateForm(app.template.sheet);
-  updateSheet(app.crunch);
-  //doCalculations(app.crunch);
+  loadDataFromFile(app.data);
+  doCalculations(app);
+  exportToJsonFile(app.data);
 }
 
-function generateCrunch(template) {
-  let crunch = {};
+function cacheData(template) {
+  let data = {};
+  let calc = [];
   template.section.forEach(section => {
     section.group.forEach(group => {
       group.field.forEach(field => {
         if (field.value.length == 1) {
-          crunch[field.name] = characterData["sheet"][field.name];
+          data[field.name] = characterData["sheet"][field.name];
         } else {
           for (let i = 0; i < field.value.length; i++) {
-            if ((characterData["sheet"][field.name][i]) == null) {
-              crunch[field.name + i] = field.value[i];
+            if ((characterData["sheet"][field.name][i]) == null) {//formula
+              let fv = field.value[i];
+              calc.push({ "name": field.name + i, "formula": fv.match(/[^:]*$/gi)[0], "order": Number(fv.match(/^[0-9]+/)[0]) });
+              data[field.name + i] = "";
             } else {
-              crunch[field.name + i] = characterData["sheet"][field.name][i];
+              data[field.name + i] = characterData["sheet"][field.name][i];
             }
           }
         }
       });
     });
   });
-  console.log("crunch: ", crunch);
-
-  app.crunch = crunch
+  calc.sort((a, b) => a.order - b.order);
+  app.data = data;
+  app.calc = calc;
+  console.log(app);
 }
 
 function generateForm(sheet) {
@@ -55,30 +67,47 @@ function generateForm(sheet) {
   });
 }
 
-function updateSheet(crunch) {
-  Object.keys(crunch).forEach(field => {
-    if (field.includes("+") || field.includes("*")) { //calculation field
-      document.getElementsByName(field)[0].value = doSingleCalc(crunch[field], crunch)
-    } else {
-      //FIXME: the fields that fail are the ones that do not have *+ but the value are numbers.
-      console.log(document.getElementsByName(field));
-      try {
-        document.getElementsByName(field)[0].value = crunch[field];
-      } catch (error) {
-        console.log("Field not found in template: " +
-          field);
-      }
+
+function loadDataFromFile(data) {
+  Object.keys(data).forEach(dataField => {
+    let formField;
+    try {
+      formField = document.getElementsByName(dataField)[0];
+    } catch (error) {
+      console.log("Field not found in template: " + dataField);
     }
+    formField.value = data[dataField];
   });
 }
 
-function doSingleCalc(field, crunch) {
+
+function doCalculations(data) {
+  console.log("Doing calculations.");
+  data.calc.forEach(dataField => {
+    let formField;
+    try {
+      formField = document.getElementsByName(dataField.name)[0];
+    } catch (error) {
+      console.log("Calculation not found in template: " + dataField);
+    }
+    formField.value = doSingleCalc(dataField.formula, app.data);
+    app.data[formField.name] = Number(formField.value);
+  });
+}
+
+function doSingleCalc(field, data) {
   if (field.includes("+")) {
     let sum = 0;
     field.split("+").forEach(element => {
-      sum += crunch[element];
+      sum += data[element];
     });
     return sum
+  } else {
+    let mult = 0;
+    field.split("*").forEach(element => {
+      mult *= data[element];
+    });
+    return mult
   }
 }
 
@@ -94,8 +123,8 @@ function newSheet(config) {
 }
 
 function newSection(config) {
-  let section = document.createElement("div");
-  let title = document.createElement("div");
+  let section = document.createElement("fieldset");
+  let title = document.createElement("legend");
   title.className = "title";
   title.title = config.label.value;
   title.innerHTML = config.label.value;
@@ -120,13 +149,11 @@ function newGroup(config) {
   return group;
 }
 
-function addButton() {
-  let button = document.createElement("input");
-  button.type = "submit";
-  button.name = "save";
-  button.text = "save";
-  button.onclick = saveData;
-  //  sheet.appendChild(button);
+
+
+function saveData() {
+  localStorage.setItem("characterData", JSON.stringify(app.data));
+  alert("Saved!");
 }
 
 function newField(config) {
@@ -137,12 +164,19 @@ function newField(config) {
     fieldGroup.appendChild(newLabel(config.label))
   }
   let c = 0;
-  config.value.forEach(element => {
-    let field = document.createElement("input");
-    field.value = element;
+  config.value.forEach(value => {
+    let field;
+    field = document.createElement("input");
+    if (typeof (value) == 'string') {
+      field.className = "result";
+      field.disabled = true;
+    } else {
+      field = document.createElement("input");
+      field.className = "field";
+    }
+    field.value = value;
     field.type = config.type;
-
-    if (field.type == "number") {
+    if (config.value.length > 1) {
       field.name = config.name + c;
     } else {
       field.name = config.name
@@ -151,11 +185,12 @@ function newField(config) {
     field.size = config.size;
     field.style.width = config.size + "rem";
     field.maxLength = config.size;
-    field.className = "field";
+
     field.onchange = () => {
       if (field.value != "") {
-        app.crunch[field.name] = Number(document.getElementsByName(field.name)[0].value);
-        updateSheet(app.crunch);
+        app.data[field.name] = Number(document.getElementsByName(field.name)[0].value);
+        doCalculations(app);
+        console.log(app.data)
       }
     }
     fieldGroup.appendChild(field);
@@ -184,5 +219,14 @@ function newLabelResult(value) {
   return label;
 }
 
+function exportToJsonFile(jsonData) {
+  let dataStr = JSON.stringify(jsonData);
+  let dataUri = 'data:application/json;charset=utf-8,' + encodeURIComponent(dataStr);
+  let exportFileDefaultName = jsonData.basics_name + ".json";
+  let linkElement = document.getElementsByName("export")[0];
+  linkElement.setAttribute('href', dataUri);
+  linkElement.setAttribute('download', exportFileDefaultName);
+  // document.body.appendChild(linkElement);
+}
 
 window.onload = init;
--- templates.js
+++ templates.js
@@ -12,9 +12,11 @@ let characterData = {
     "basics_hair": "Dark",
     "basics_eyes": "Brown",
     "ws": [1, 2],
-    "bs": [3, 4]
+    "bs": [3, 4],
+    "xs": [3, 4],
+    "ys": [3, 4]
   },
-  "metadata": {
+  "meta": {
     "character_created": "",
     "character_updated": "",
     "template_name": "",
@@ -33,158 +35,194 @@ let templates = {
         "row": "1",
         "col": "1-10",
         "label": {
-          "value": "Characteristics",
+          "value": "basic information",
           "format": "capitalize",
           "font": "CormorantInfant-Medium.ttf"
         },
         "group": [{
           "orientation": "row",
           "field": [{
-              "name": "basics_name",
-              "value": [, ],
-              "orientation": "column",
-              "type": "text",
-              "size": 3,
-              "label": {
-                "position": "last",
-                "value": "name",
-                "format": "capitalize",
-                "size": 3
-              }
-            },
-            {
-              "name": "basics_species",
-              "value": [, ],
-              "orientation": "column",
-              "type": "text",
-              "size": 3,
-              "label": {
-                "position": "last",
-                "value": "species",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_class",
-              "value": [, ],
-              "orientation": "column",
-              "type": "text",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_career",
-              "value": [, ],
-              "orientation": "column",
-              "type": "text",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_career_level",
-              "value": [, ],
-              "orientation": "column",
-              "type": "number",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_career_path",
-              "value": [, ],
-              "orientation": "column",
-              "type": "text",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_status",
-              "value": [, ],
-              "orientation": "column",
-              "type": "text",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_age",
-              "value": [, ],
-              "orientation": "column",
-              "type": "number",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_height",
-              "value": [, ],
-              "orientation": "column",
-              "type": "number",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_hair",
-              "value": [, ],
-              "orientation": "column",
-              "type": "text",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "basics_eyes",
-              "value": [, ],
-              "orientation": "column",
-              "type": "text",
-              "size": 1,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 2
-              }
-            }, {
-              "name": "ws",
-              "value": [, , "ws0+ws1"],
-              "orientation": "column",
-              "type": "number",
-              "size": 3,
-              "label": {
-                "position": "last",
-                "value": "ws",
-                "format": "capitalize",
-                "size": 3
-              }
-            },
+            "name": "basics_name",
+            "value": [,],
+            "orientation": "column",
+            "type": "text",
+            "size": 3,
+            "label": {
+              "position": "last",
+              "value": "name",
+              "format": "capitalize",
+              "size": 3
+            }
+          },
+          {
+            "name": "basics_species",
+            "value": [,],
+            "orientation": "column",
+            "type": "text",
+            "size": 3,
+            "label": {
+              "position": "last",
+              "value": "species",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_class",
+            "value": [,],
+            "orientation": "column",
+            "type": "text",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "ws",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_career",
+            "value": [,],
+            "orientation": "column",
+            "type": "text",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "ws",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_career_level",
+            "value": [,],
+            "orientation": "column",
+            "type": "number",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "ws",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_career_path",
+            "value": [,],
+            "orientation": "column",
+            "type": "text",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "ws",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_status",
+            "value": [,],
+            "orientation": "column",
+            "type": "text",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "ws",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_age",
+            "value": [,],
+            "orientation": "column",
+            "type": "number",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "ws",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_height",
+            "value": [,],
+            "orientation": "column",
+            "type": "number",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "ws",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_hair",
+            "value": [,],
+            "orientation": "column",
+            "type": "text",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "hair",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "basics_eyes",
+            "value": [,],
+            "orientation": "column",
+            "type": "text",
+            "size": 1,
+            "label": {
+              "position": "last",
+              "value": "eyes",
+              "format": "capitalize",
+              "size": 2
+            }
+          }, {
+            "name": "ws",
+            "value": [, , "0:ws0+ws1"],
+            "orientation": "column",
+            "type": "number",
+            "size": 3,
+            "label": {
+              "position": "last",
+              "value": "ws",
+              "format": "capitalize",
+              "size": 3
+            }
+          }, {
+            "name": "bs",
+            "value": [, , "1:bs0+bs1+ws2+xs2"],
+            "orientation": "column",
+            "type": "number",
+            "size": 3,
+            "label": {
+              "position": "last",
+              "value": "bs",
+              "format": "capitalize",
+              "size": 3
+            }
+          }, {
+            "name": "xs",
+            "value": [, , "1:xs0+xs1+ws2"],
+            "orientation": "column",
+            "type": "number",
+            "size": 3,
+            "label": {
+              "position": "last",
+              "value": "xs",
+              "format": "capitalize",
+              "size": 3
+            }
+          }, {
+            "name": "ys",
+            "value": [, , "1:ys0+ys1+ws2"],
+            "orientation": "column",
+            "type": "number",
+            "size": 3,
+            "label": {
+              "position": "last",
+              "value": "ys",
+              "format": "capitalize",
+              "size": 3
+            }
+          },
           ]
         }]
       }] //section
